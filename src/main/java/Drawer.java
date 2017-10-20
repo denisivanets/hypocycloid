228e73c8403ee0a8bc648601eac2f3a894025b53
DIvanets <denisivanets@gmail.com>
2017-10-21 05:06:23 +0600

    almost complete

--- src/main/java/Drawer.java
+++ src/main/java/Drawer.java
@@ -1,30 +1,46 @@
-import lombok.RequiredArgsConstructor;
+
+import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.SneakyThrows;
 import lombok.val;
 
 import javax.swing.*;
 import java.awt.*;
 
-@RequiredArgsConstructor
+@NoArgsConstructor
 public class Drawer extends JPanel {
 
-    private final PointProvider provider;
     @Setter
-    private int sizeCoeff;
+    private PointProvider provider;
 
     @Override
-    @SneakyThrows
     public void paint(Graphics g) {
+        if (provider == null) {
+            return;
+        }
+        int sizeCoeff = calculateCoeff();
+        int transitionCoords = 300;
         val points = provider.providePoints();
         for (int i = 0; i < points.size() - 1; i++) {
             val firstPoint = points.get(i);
             val secondPoint = points.get(i + 1);
-            int x1 = (int) (firstPoint.getX() * 50) + 300;
-            int x2 = (int) (secondPoint.getX() * 50) + 300;
-            int y1 = (int) (firstPoint.getY() * 50) + 300;
-            int y2 = (int) (secondPoint.getY() * 50) + 300;
+            int x1 = (int) (firstPoint.getX() * sizeCoeff) + transitionCoords;
+            int x2 = (int) (secondPoint.getX() * sizeCoeff) + transitionCoords;
+            int y1 = (int) (firstPoint.getY() * sizeCoeff) + transitionCoords;
+            int y2 = (int) (secondPoint.getY() * sizeCoeff) + transitionCoords;
             g.drawLine(x1, y1, x2, y2);
         }
+    }
+
+    public void clearAndRepaint(final Graphics g) {
+        g.clearRect(0,0,this.getWidth(),this.getHeight());
+        repaint();
+    }
+
+    private int calculateCoeff() {
+        double standardValue = 3;
+        double defaultCoeff = 80;
+        val firstPoint = this.provider.providePoints().get(0);
+        double valuesRatio = firstPoint.getX() / standardValue;
+        return (int) (Math.round(defaultCoeff / valuesRatio));
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import lombok.val;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
 
@@ -15,9 +16,25 @@ public class Main {
         frame = new JFrame("Hypocycloid");
         frame.setSize(1000, 700);
         frame.setVisible(true);
-        val pointsProvider = new HypocycloidPointsProvider(1, 6);
-        val drawer = new Drawer(pointsProvider);
+        JTextField rField = new JTextField("r", 10);
+        JTextField RField = new JTextField("R", 10);
+        JButton button = new JButton("Make");
+        JPanel utilPanel = new JPanel();
+        utilPanel.add(rField);
+        utilPanel.add(RField);
+        utilPanel.add(button);
+        frame.getContentPane().add(BorderLayout.EAST, utilPanel);
+        val drawer = new Drawer();
         frame.getContentPane().add(drawer);
+        button.addActionListener((event) -> {
+            final int r = Integer.parseInt(rField.getText());
+            final int R = Integer.parseInt(RField.getText());
+            val pointsProvider = new HypocycloidPointsProvider(r, R);
+            drawer.setProvider(pointsProvider);
+            drawer.clearAndRepaint(drawer.getGraphics());
+            frame.setVisible(false);
+            frame.setVisible(true);
+        });
     }
 
 }
